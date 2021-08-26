1da129d6f7d94bac3a02777b580b4a1f90e1ae4a
Brinton <brintonfoyreed@gmail.com>
2021-08-26 12:07:14 -0700

    Controller setup

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -1,2 +1,23 @@
 class UserPostsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    def index
+        user_posts = UserPost.all
+        render json: user_posts
+    end
+
+    def show
+        user_posts = find_user_posts
+        render json: user_posts, include: :user_posts
+    end
+
+    private
+
+    def find_user_posts
+        UserPost.find_by(id: params[:id]) 
+    end
+
+    def render_not_found_response
+        render json: { error: "Record not found" }, status: :not_found
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = find_user
+        render json: user, include: :user
+    end
+
+    def me
+        user = find_user
+        if user
+            render json: user
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def find_user
+        User.find_by(id: params[:id]) 
+    end
+
+    def render_not_found_response
+        render json: { error: "User not found" }, status: :not_found
+    end
 end
