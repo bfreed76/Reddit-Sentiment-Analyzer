3f52065152bb7f1cc8f04da4ef99c002cd153dc9
Brinton <brintonfoyreed@gmail.com>
2021-08-27 10:27:40 -0700

    Create initial React components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to my App!
         </p>
--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
