42544efc3bfc1528e25d62d1fd9254178cf1cc8c
Brinton <brintonfoyreed@gmail.com>
2021-09-11 15:47:58 -0700

    Results container ready for semantic.iu cards

--- client/src/Components/ResultsContainer.js
+++ client/src/Components/ResultsContainer.js
@@ -3,9 +3,29 @@ import { Context } from "../context/Context";
 
 const ResultsContainer = () => {
     const { user, results } = useContext(Context);
+    
+    debugger
+    
     return (
         <div>
             RESULTS CARDS HERE
+            {/* 
+            (Header)
+            results.user
+            user.results_joins[user.results_joins.length-1].created_at
+            results.author.name
+            results.subreddit.name
+            results.searchTerms.search_term  (check for search terms)
+            results.sentimentDocument
+            results.emotionDocument
+            results.emotionTarget
+
+            (Cards)
+            results.data[1].author
+            results.data[1].body
+            results.data[1].score
+            results.data[1].created_utc
+            */}
         </div>
     )
 }
