2c8d9b77b296ecf7693b6219edaed371c465b1db
Brinton <brintonfoyreed@gmail.com>
2021-09-21 13:33:21 -0700

    heroku prob 2

--- client/src/Components/MySearches.js
+++ client/src/Components/MySearches.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-
-import React, { useState, useEffect } from "react";
-import { Card, Feed } from "semantic-ui-react";
-import TopContentCard from "./TopContentCard";
-
-const MySearches = () => {
-    // const [mySearches, setMySearches] = useState([]);
-
-    // useEffect(() => {
-    //   getMySearches();
-    // }, []);
-  
-    // const getMySearches = () => {
-    //   fetch("/my_searches")
-    //     .then((res) => res.json())
-    //     .then((data) => {
-    //       console.log("My Searches: ", data);
-    //       setMySearches(data);
-    //     })
-    //     .catch((err) => console.log("error =", err));
-    // };
-  
-    return (
-      <div> 
-          <h1>HERE</h1>
-          //ALL DATA STORED IN USER
-        {/* {mySearches.map((item) => {
-         return <TopContentCard item={item} />
-        })} */}
-      </div>
-    );
-  };
-
-export default MySearches
-=======
 import React, { useState, useEffect } from "react";
 import MySearchCards from "./MySearchCards";
 
@@ -67,4 +31,3 @@ const MySearches = () => {
 };
 
 export default MySearches;
->>>>>>> graphs
