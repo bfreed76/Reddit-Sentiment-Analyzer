bab73c85fca9a8cb3df60f1f7edd02666450dba8
Brinton <brintonfoyreed@gmail.com>
2021-09-23 13:30:40 -0700

    Heroku deployment effort 2.8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,30 @@
-class ApplicationController < ActionController::Base
-  include ActionController::Cookies
-  # Adding CSRF cookies for deployment to Heroku
-  skip_before_action :verify_authenticity_token
-  # after_action :set_csrf_cookie
+module Api 
 
-  # Fallback route
+  class ApplicationController < ActionController::Base
+    include ActionController::Cookies
+    # Adding CSRF cookies for deployment to Heroku
+    skip_before_action :verify_authenticity_token
+    # after_action :set_csrf_cookie
 
-  def fallback_index_html
-    render file: 'public/index.html'
-  end
+    # Fallback route
 
-  private
+    def fallback_index_html
+      render file: 'public/index.html'
+    end
 
-  # CSRF token creation for deployment to Heroku
+    private
 
-  def set_csrf_cookie
-    cookies["CSRF-TOKEN"] = {
-      value: form_authenticity_token,
-      secure: true,
-      same_site: :strict,
-      domain: 'reddit-emotion-analyzer.herokuapp.com/'}
-      # p 'cookie  ****' + cookies["CSRF-TOKEN"]
+    # CSRF token creation for deployment to Heroku
+
+    def set_csrf_cookie
+      cookies["CSRF-TOKEN"] = {
+        value: form_authenticity_token,
+        secure: true,
+        same_site: :strict,
+        domain: 'reddit-emotion-analyzer.herokuapp.com/'}
+        # p 'cookie  ****' + cookies["CSRF-TOKEN"]
+    end
+
   end
 
 end
